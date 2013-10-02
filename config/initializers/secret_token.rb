14aa5521fe533c38ae649b1175d6f01fa45051f7
Corbin Sykes <corbin.sykes@gmail.comm>
2013-10-02 17:52:00 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabbleIi::Application.config.secret_token = '8e9a85b5e4f07789c660e195038a0f6d043a5e68850046f56fae005a120797ea5926ea1fdb954340e53a517148122bf389bdf7ed6cd0a1b1d69445ccb49e9c35'
